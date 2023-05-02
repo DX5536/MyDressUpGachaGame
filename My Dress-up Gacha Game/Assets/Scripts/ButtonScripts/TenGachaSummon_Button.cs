29fbc13719ba60f98556ed9da1d6e7988e473c0d
Hai Lam Dang <hai.dang@ue-germany.de>
2023-05-02 20:11:59 +0200

    [CHANGE] 10x summon now working as intended

--- My Dress-up Gacha Game/Assets/Scripts/ButtonScripts/TenGachaSummon_Button.cs
+++ My Dress-up Gacha Game/Assets/Scripts/ButtonScripts/TenGachaSummon_Button.cs
@@ -37,28 +37,13 @@ public class TenGachaSummon_Button: MonoBehaviour
     private int perCent;
     private int pullCount = 10;
 
-    //local var to save writing time.
-    private int local_10xSavedBannerIndex;
-
     //GachaSummon_Button already have the logic of Banner selection
     //So we will skip that here
     //We will take the BannerIndex (from the chosen banner) through the var GachaSummon_ButtonScript
 
     private void Start()
     {
-        local_10xSavedBannerIndex = gachaSummon_ButtonScript.SavedBannerIndex;
         summon10x_Panel.SetActive(false);
-        CopyAndPast10xPanelChildrenToArray();
-    }
-
-    //This method should auto copy children from summon10x_Panel to array
-    private void CopyAndPast10xPanelChildrenToArray()
-    {
-        for (int i = 0;i < gainedItem10x_TMP.Length;i++)
-        {
-            gainedItem10x_TMP[i] = summon10x_Panel.GetComponentInChildren<TextMeshProUGUI>();
-            gainedItem10x_IMG[i] = summon10x_Panel.GetComponentInChildren<Image>();
-        }
     }
 
     //public method to access in Summon! 10x button
@@ -106,63 +91,50 @@ public class TenGachaSummon_Button: MonoBehaviour
                 Debug.LogError("ERROR! It should not be possible to go below 0% or above 100% -> Check ScriptableObject");
             }
 
-            //ExecuteSelectedBanner();
-            //EnableGainedItemUI(true);
-
+            //The item will be displayed on the according slot [i]
             BannerDisplayItem(i);
-
-            //Reduce SummonTicketAmount in SO by 10 after summoning
-            currencyScriptableObject.SummonTicketAmount -= 10;
-
-            //Then check if there is still enough summonTicket
-            CheckEnoughSummonTicket();
         }
 
+        //Reduce SummonTicketAmount in SO by 10 after summoning
+        currencyScriptableObject.SummonTicketAmount -= 10;
 
+        //Then check if there is still enough summonTicket
+        CheckEnoughSummonTicket();
     }
-
-    private void ExecuteSelectedBanner()
-    {
-        //CopyPasteSOValuesToLocalValues(savedBannerIndex);
-        //BannerDisplayItem(10);
-    }
-
 
     //This will display the gained item (text + sprites) on the UI
     //I have to make a parameter as this will feed into SummonLogic
     //By making them dependent -> We know where something fail
     private void BannerDisplayItem(int bannerDisplayItem_Index)
     {
-        //First create a loop (10 times) that will go through the display of gained items
-        for (bannerDisplayItem_Index = 0;bannerDisplayItem_Index < pullCount;bannerDisplayItem_Index++)
+        //We do not need to create another loop
+        //As this method will be call 1 time each time a loop in SummonLogic procs
+        GainedItem_TweenAnimation(bannerDisplayItem_Index);
+
+        switch (gainedItem_Index)
         {
-            GainedItem_TweenAnimation(bannerDisplayItem_Index);
-
-            switch (gainedItem_Index)
-            {
-                case 0:
-                    gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[0];
-                    gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[0];
-                    break;
-                case 1:
-                    gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[1];
-                    gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[1];
-                    break;
-                case 2:
-                    gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[2];
-                    gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[2];
-                    break;
-                case 3:
-                    gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[3];
-                    gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[3];
-                    break;
-                case 4:
-                    gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[4];
-                    gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[4];
-                    break;
-            }
+            case 0:
+                gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[0];
+                gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[0];
+                break;
+            case 1:
+                gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[1];
+                gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[1];
+                break;
+            case 2:
+                gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[2];
+                gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[2];
+                break;
+            case 3:
+                gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[3];
+                gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[3];
+                break;
+            case 4:
+                gainedItem10x_TMP[bannerDisplayItem_Index].text = gachaSummon_ButtonScript.LocalItemNames[4];
+                gainedItem10x_IMG[bannerDisplayItem_Index].sprite = gachaSummon_ButtonScript.LocalItemImages[4];
+                break;
         }
-
+        Debug.Log("Display gainedItem10x_TMP/IMG at index " + bannerDisplayItem_Index);
     }
 
 
@@ -185,15 +157,10 @@ public class TenGachaSummon_Button: MonoBehaviour
 
     private void GainedItem_TweenAnimation(int GainedItem_TweenAnimation_Index)
     {
-        //First create a loop (10 times) that will go through the display of gained items
-        for (GainedItem_TweenAnimation_Index = 0;GainedItem_TweenAnimation_Index < pullCount;GainedItem_TweenAnimation_Index++)
-        {
+        gainedItem10x_TMP[GainedItem_TweenAnimation_Index].transform.localScale = Vector3.zero;
+        gainedItem10x_IMG[GainedItem_TweenAnimation_Index].color = new Color(255, 255, 255, 0);
 
-            gainedItem10x_TMP[GainedItem_TweenAnimation_Index].transform.localScale = Vector3.zero;
-            gainedItem10x_IMG[GainedItem_TweenAnimation_Index].color = new Color(255, 255, 255, 0);
-
-            gainedItem10x_TMP[GainedItem_TweenAnimation_Index].DOScale(tweenScale, tweenValuesScriptableObject.TweenDuration);
-            gainedItem10x_IMG[GainedItem_TweenAnimation_Index].DOFade(1, tweenValuesScriptableObject.TweenDuration);
-        }
+        gainedItem10x_TMP[GainedItem_TweenAnimation_Index].DOScale(tweenScale, tweenValuesScriptableObject.TweenDuration);
+        gainedItem10x_IMG[GainedItem_TweenAnimation_Index].DOFade(1, tweenValuesScriptableObject.TweenDuration);
     }
 }
